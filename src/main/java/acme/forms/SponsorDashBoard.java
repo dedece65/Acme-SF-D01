cfd276c0e0040ac1f5eccdbfece864fb937b8ca6
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-02-20 22:13:44 +0100

    [feat] add SponsorDashBoard entity

---
+++ src/main/java/acme/forms/SponsorDashBoard.java
@@ -0,0 +1,25 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SponsorDashBoard extends AbstractForm {
+
+	private static final long	serialVersionUID	= 1L;
+
+	Integer						invoicesWithTaxLessEqual21;
+	Integer						sponsorShipsWithLink;
+	Double						averageAmountSponsorShips;
+	Double						deviationAmountSponsorShips;
+	Double						minimunAmountSponsorShips;
+	Double						maximumAmountSponsorShips;
+	Double						averageQuantityInvoices;
+	Double						deviationQuantityInvoices;
+	Double						minimunQuantityInvoices;
+	Double						maximumQuantityInvoices;
+
+}
