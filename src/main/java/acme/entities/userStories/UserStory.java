ab24602d66ac7f18fd15bab4e04d6a77575228b2
Jaime-Linares <jailinbar@alum.us.es>
2024-02-23 20:51:08 +0100

    [fix] some changes in userStory entity

--- src/main/java/acme/entities/userStories/UserStory.java
+++ src/main/java/acme/entities/userStories/UserStory.java
@@ -3,8 +3,10 @@ package acme.entities.userStories;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Min;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -32,20 +34,24 @@ public class UserStory extends AbstractEntity {
 	@Length(max = 101)
 	private String				description;
 
-	@Min(0)
-	private Double				estimatedCost;   // preguntar tipo (podria ser Date) y restricciones
+	@NotNull
+	@Positive
+	private Double				estimatedCost;
 
 	@NotBlank
 	@Length(max = 101)
 	private String				acceptanceCriteria;
 
+	@NotNull
 	private Priority			priority;
 
-	private String				optional;
+	private String				link;
 
-	// Relations ---------------------------------------
+	// Relationships ---------------------------------------
 
-	@ManyToOne()  // las historias de usuario tiene que tener siempre un proyecto?
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
 	private Project				project;
 
 }
