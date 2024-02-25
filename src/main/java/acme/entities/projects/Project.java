05ccedf329fbfabe55fc4f059b7180ba95ad9971
Jaime-Linares <jailinbar@alum.us.es>
2024-02-25 19:58:47 +0100

    [fix] change some restrictions of some attributes

--- src/main/java/acme/entities/projects/Project.java
+++ src/main/java/acme/entities/projects/Project.java
@@ -31,11 +31,11 @@ public class Project extends AbstractEntity {
 	private String				code;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	private String				title;
 
 	@NotBlank
-	@Length(max = 101)
+	@Length(max = 100)
 	private String				$abstract;
 
 	@NotNull
