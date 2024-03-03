52b093c5ac134ab11c070468909abf8741237055
Jaime-Linares <jailinbar@alum.us.es>
2024-03-03 22:03:31 +0100

    [feat] objective entity and priority enum created

---
+++ src/main/java/acme/entities/objectives/Objective.java
@@ -0,0 +1,68 @@
+
+package acme.entities.objectives;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Objective extends AbstractEntity {
+
+	// Serialisation identifier ----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes ------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				tile;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@NotNull
+	private Priority			priority;
+
+	private boolean				status;
+
+	@Min(1)
+	private int					duration;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
+
+}
