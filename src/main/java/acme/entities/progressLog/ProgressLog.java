05f435f9e87b6c87e07b45334f519bee5cb558af
Enrgaraba <enrgaraba@alum.us.es>
2024-03-03 14:51:21 +0100

    [fix] Imports correctiong in ProcessLog entity

--- src/main/java/acme/entities/progressLog/ProgressLog.java
+++ src/main/java/acme/entities/progressLog/ProgressLog.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -16,6 +18,7 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.contract.Contract;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -56,14 +59,10 @@ public class ProgressLog extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-	/*
-	 * QUE COÑO ES ESTO KIQUE
-	 * 
-	 * @NotNull
-	 * 
-	 * @Valid
-	 * 
-	 * @ManyToOne(optional = false)
-	 * private Contract contract;
-	 */
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Contract			contract;
+
 }
