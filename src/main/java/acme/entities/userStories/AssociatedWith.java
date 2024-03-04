09bdd2c5dadc1cc7ae27f0e706f99671fbfa2e49
Jaime-Linares <jailinbar@alum.us.es>
2024-03-04 21:46:58 +0100

    [fix] user story relation with project, explicit table (AssociatedWith)

---
+++ src/main/java/acme/entities/userStories/AssociatedWith.java
@@ -0,0 +1,42 @@
+
+package acme.entities.userStories;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AssociatedWith extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// Derived attributes -----------------------------------------------------
+
+	@NotNull
+	private boolean				published;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private UserStory			userStroy;
+
+}
--- src/main/java/acme/entities/userStories/UserStory.java
+++ src/main/java/acme/entities/userStories/UserStory.java
@@ -2,8 +2,6 @@
 package acme.entities.userStories;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -12,7 +10,6 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.entities.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,6 +32,7 @@ public class UserStory extends AbstractEntity {
 	@Length(max = 100)
 	private String				description;
 
+	@NotNull
 	@Min(1)
 	private int					estimatedCost;
 
@@ -46,17 +44,11 @@ public class UserStory extends AbstractEntity {
 	private Priority			priority;
 
 	@URL
+	@Length(max = 255)
 	private String				link;
-
-	private boolean				published;
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	private Project				project;
 
 }
