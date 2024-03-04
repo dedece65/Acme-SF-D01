7d4b71bb09505d70f25ca3deb29ab5752e19137b
IbaiPerez <ibaperfer@alum.us.es>
2024-03-04 15:03:32 +0100

    [feat] Store banner information in the database

---
+++ src/main/java/acme/entities/banner/Banner.java
@@ -0,0 +1,58 @@
+
+package acme.entities.banner;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	// Serialisation identifier ----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes ------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	private Date				instantiationMoment;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	private Date				displayStartDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	private Date				displayEndDate;
+
+	@NotBlank
+	private String				picture;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				slogan;
+
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
