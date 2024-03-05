890fe5f381556c15a073ffa146dccc01aeee3013
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-03-05 18:08:52 +0100

    [feat] Notice entity

---
+++ src/main/java/acme/entities/notice/Notice.java
@@ -0,0 +1,62 @@
+
+package acme.entities.notice;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Notice extends AbstractEntity {
+
+	// Serialisation identifier ----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes ------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	protected Date				instantiationMoment;
+
+	@NotNull
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotNull
+	@NotBlank
+	@Length(max = 75)
+	private String				author;
+
+	@NotNull
+	@NotBlank
+	@Length(max = 100)
+	private String				message;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+	// Derived attributes ----------------------------------------------------- 
+
+	// Relationships ----------------------------------------------------------
+
+}
