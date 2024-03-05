6bea72a1c814814688caa91cdfa34b6b05d854e3
Jaime-Linares <jailinbar@alum.us.es>
2024-03-05 17:47:15 +0100

    [feat] relation between project and user story done

--- src/main/java/acme/entities/userStories/UserStory.java
+++ src/main/java/acme/entities/userStories/UserStory.java
@@ -2,6 +2,8 @@
 package acme.entities.userStories;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -10,6 +12,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.roles.Manager;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -50,5 +53,10 @@ public class UserStory extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Manager				manager;
 
 }
