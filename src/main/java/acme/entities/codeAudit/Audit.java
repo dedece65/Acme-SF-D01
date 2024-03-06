c871358bf94e95e0ff0e9db33ec754049128c24a
IbaiPerez <ibaperfer@alum.us.es>
2024-03-06 20:22:48 +0100

    [fix] Message added

--- src/main/java/acme/entities/codeAudit/Audit.java
+++ src/main/java/acme/entities/codeAudit/Audit.java
@@ -35,7 +35,7 @@ public class Audit extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$", message = "{validation.project.code}")
 	private String				code;
 
 	@Temporal(TemporalType.TIMESTAMP)
