5e24e7642b0e3e8a605ab06ac846a2fc956203c7
IbaiPerez <ibaperfer@alum.us.es>
2024-03-06 10:45:33 +0100

    [fix]Few error solved

--- src/main/java/acme/entities/codeAudit/Audit.java
+++ src/main/java/acme/entities/codeAudit/Audit.java
@@ -1,12 +1,10 @@
 
 package acme.entities.codeAudit;
 
-import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
@@ -23,31 +21,25 @@ import lombok.Setter;
 @Setter
 public class Audit extends AbstractEntity {
 
-	private static final long				serialVersionUID	= 1L;
+	private static final long	serialVersionUID	= 1L;
 
 	@Column(unique = true)
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
-	private String							code;
+	private String				code;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	@NotNull
-	private Date							executionDate;
+	private Date				executionDate;
 
 	@NotNull
-	private AuditType						type;
-
-	@OneToMany(mappedBy = "audit")
-	private Collection<AuditRecord>			auditRecords;
-
-	@OneToMany(mappedBy = "audit")
-	private Collection<CorrectiveActions>	correctiveActions;
+	private AuditType			type;
 
 	//	@NotBlank
 	//	@Length(max = 101)
 	//	private String				correctiveActions;
 
-	private String							link;
+	private String				link;
 
 }
